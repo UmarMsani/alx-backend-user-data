f5c30a5fad52013b85a4b38592486ccb1df7d1e0
UmarMsani <umuhammad220@gmail.com>
2024-04-11 09:36:24 +0000

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -34,3 +34,10 @@ def is_valid(stored_pwd: bytes, input_pwd: str) -> bool:
         return a bool
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
