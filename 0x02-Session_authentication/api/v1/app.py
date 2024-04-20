f1b610176caca2691837b1f41dbe815d51435ee0
UmarMsani <umuhammad220@gmail.com>
2024-04-20 00:16:40 -0700

    Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -71,7 +71,7 @@ def bef_req():
         return
 
     if auth.authorization_header(request) is None \
-        and auth.session_cookie(request) is None:
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
